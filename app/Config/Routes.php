dd5d17a5aae88cd9f74e4c56bfa8ea454f03be9d
Andrei <thesemeth@gmail.com>
2026-01-12 15:05:41 +0200

    bigbang

---
+++ app/Config/Routes.php
@@ -0,0 +1,16 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+
+$routes->get('news', 'News::index');
+$routes->get('news/new', 'News::new');
+$routes->post('news', 'News::create');
+$routes->get('news/(:segment)', 'News::show/$1');
+
+$routes->get('pages', 'Pages::index');
+$routes->get('(:segment)', 'Pages::view/$1');
